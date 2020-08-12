b31fc11656c226fcb914bc8378ab6ef3fa83d339
Thomas Kutschera <tkutsch99@gmail.com>
2020-08-12 17:21:01 +0200

    Create tkVirtualProductionHelperUI.py

---
+++ tkVirtualProductionHelperUI.py
@@ -0,0 +1,505 @@
+# tkVirtualProductionHelper.py
+import maya.cmds as cmds
+import maya.mel as mel
+from functools import partial  
+# import thomas as tk
+
+'''
+Tools for zorro sim
+---------------------------------------------------
+
+
+'''
+ver                 = '1.0'
+
+colDarkGreen    = [0.1, 0.16, 0.08]
+colFrame        = [0.3, 0.3, 0.3]
+colFrameRed     = [0.4, 0.3, 0.3]
+colDarkRed      = [0.32, 0.2, 0.14]
+colFrameGreen   = [0.3, 0.4, 0.28]
+colLightGreen   = [0.4, 0.5, 0.36]
+colDark         = [0.1, 0.1, 0.1]
+colBlue         = [0.32, 0.2, 0.14]
+colBrown        = [0.1, 0.16, 0.08]
+
+defaultPath     = 'X:/zorro_zor-5069/_library/assets/characters/chr_zorroFox/cfx_groom/'
+approvedxGen    = 'zor_chr_zorroFox_cfx_groom_v080_thk__body_collection'
+xGenBaseGeo     = 'body_cn_hi_cfx'
+dynControl       = 'dyn_GRP'
+
+windowStartHeight   = 50
+windowStartWidth    = 200
+bh1                 = 24
+
+def tkHelpSwitchInstancerInputs(*args):
+    if cmds.window('win_tkSwitchInstancerINPUtsHELP', exists=1):
+        cmds.deleteUI('win_tkSwitchInstancerINPUtsHELP')
+    myWindow = cmds.window('win_tkSwitchInstancerINPUtsHELP', s=1, t='help', wh=[200, 200])
+    helpText = 'Replace instanced references without changing order\n---------------------------------------------------\nInstancer:          Choose Instancer \nUses                   The namespace of the first connected    obj\nReplace With:   Select file you want to replace from\n                           Gets the namespace automatically'
+    cmds.columnLayout(adj=1)
+    cmds.text(helpText, al='left')
+    cmds.showWindow(myWindow )
+
+
+
+def cShrinkWin(windowToClose, *args):
+    cmds.window(windowToClose, e=1, h=20)
+    cmds.window(windowToClose, e=1, w=420)
+
+
+def tk_linkAnimToCloth(action, *args):
+    CLT = ['cape_cn_cfx_mid_geo_oneSided', 'shirt_cn_cfx_mid_geo_oneSided', 
+        'cape_cn_cfx_hi_geo_oneSided', 'shirt_cn_cfx_hi_geo_oneSided', 
+        'leatherSheath_cn_lo_geo', 'metalSheath_cn_lo_geo', 'beltBuckle_cn_lo_geo', 
+        'belt_cn_lo_geo', 'beltSheathLong_cn_lo_geo', 'beltSheathWide_cn_lo_geo', 'furVolume_cn_mid_anim_geo', 
+        'body_cn_hi_cfx']        
+    failList = []
+    fails = ''
+
+    nmSpcAnim = cmds.textField('tfNmSpcAnim', tx=1, q=1)
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1)
+    if nmSpcAnim:
+        nmSpcAnim   = nmSpcAnim + ':'
+        nmSpcFX     = nmSpcFX + ':'
+
+    for i in CLT:
+        if cmds.objExists(nmSpcFX + i) is False:
+            failList.append(i)
+        if cmds.objExists(nmSpcAnim + i) is False:
+            failList.append(i)
+
+    if len(failList) > 0:
+        print '-------------------------'
+        print 'Missing objects:'
+        for fail in failList:
+            fails += fail + ' '
+            print fail
+        cClear('tfFeedback', ('Missing objects! Check the Script Editor for details!'), [.3, 0, 0])  
+        print '-------------------------'
+
+    else:
+        print '-------------------------'
+
+        for clt in CLT:
+            # print clt
+            shapes = cmds.listRelatives(nmSpcFX + clt, s=1)    
+            
+            # disconnect
+            if action == 0: 
+                if cmds.objExists('BS_' + clt):
+                    print 'disconnecting ' + clt 
+                    bs = cmds.listConnections(shapes[0], s=1, d=0, type='blendShape')
+                    cmds.setAttr(bs[0] + '.' + clt, 0) 
+                    cmds.delete(bs) 
+            
+            # connect
+            if action == 1:
+                if not cmds.objExists('BS_' + clt):
+                    print 'connecting ' + clt
+                    cmds.select(nmSpcAnim + clt, nmSpcFX + clt)
+                    mel.eval('performBlendShape 0 1')
+                    bs = cmds.listConnections(shapes[0], s=1, d=0, type='blendShape')
+                    bs = cmds.rename(bs[0], 'BS_' + clt)
+                    # print 'bs:'
+                    # print bs
+                    cmds.setAttr(bs + '.' + clt, 1)
+                else:
+                    print (clt + ' is already connected - disconnect first!')
+
+        print '-------------------------'
+        if action == 1:
+            cClear('tfFeedback', 'Successfully linked anim to FX!', [0, 0.3, 0]) 
+        if action == 0:
+            cClear('tfFeedback', 'Successfully disconnected anim from FX!', [0, 0.3, 0]) 
+
+
+def cGetNmSpc(field, *args):
+    curSel = cmds.ls(sl=1)
+    if curSel:
+        if ':' in curSel[0]:
+            cmds.textField(field, tx=curSel[0].split(':')[0], e=1)
+        else:
+            cmds.textField(field, tx='', e=1)
+    else:
+        cmds.textField(field, tx='', e=1)
+
+
+
+def tkSetVisibility(object, *args):
+    animGrp      = 'ANIM_GRP'
+    animGrpHi    = 'ANIM_hi_GRP'
+    wrapGrp      = 'wrap_GRP'
+    collGrp      = 'passive_GRP'
+    exClothGrp   = 'export_cloth_GRP'
+    xGenBaseGrp  = 'xGenBase_GRP'
+    nmSpc        = cmds.textField('tfNmSpcAnim', tx=1, q=1)
+    nmSpcFX      = cmds.textField('tfNmSpcFX', tx=1, q=1)
+
+    if nmSpc:
+        nmSpc = nmSpc + ':'   
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if object is 'Anim':
+        low     = cmds.checkBox('cbLow', v=1, q=1)
+        mid     = cmds.checkBox('cbMid', v=1, q=1)
+        hgh     = cmds.checkBox('cbHgh', v=1, q=1)
+        but     = cmds.checkBox('cbButtons', v=1, q=1)  
+
+        if cmds.objExists(nmSpc + 'furVolume_cn_mid_anim_geo') is False:
+            cClear('tfFeedback', 'Objects not found! Check the namespace!', [.3, 0, 0])  
+
+        else:
+            dagList = cmds.ls(nmSpc + '*_lo_*', type='transform')
+            for i in dagList:
+                print i
+                cmds.setAttr(i + '.v', low)
+
+            dagList = cmds.ls(nmSpc + '*_mid_*', type='transform')
+            for i in dagList:
+                print i
+                cmds.setAttr(i + '.v', mid)
+
+            dagList = cmds.ls(nmSpc + '*_hi_*', type='transform')
+            for i in dagList:
+                print i
+                cmds.setAttr(i + '.v', hgh)
+            
+            dagList = cmds.ls(nmSpc + '*button_*', type='transform')
+            for i in dagList:
+                print i
+                cmds.setAttr(i + '.v', but)
+
+    if object is 'FX':
+        anim     = cmds.checkBox('cbAnimGRP', v=1, q=1)
+        animHi   = cmds.checkBox('cbAnimHiGRP', v=1, q=1)
+        coll     = cmds.checkBox('cbColliderGRP', v=1, q=1)
+        wrap     = cmds.checkBox('cbWrapGRP', v=1, q=1)
+        cape     = cmds.checkBox('cbCape', v=1, q=1)
+        shirt    = cmds.checkBox('cbShirt', v=1, q=1)
+        exCloth  = cmds.checkBox('cbExportClothGRP', v=1, q=1)
+        xGenBase = cmds.checkBox('cbxGenBase', v=1, q=1)
+
+        if cmds.objExists(nmSpcFX + collGrp) is False:
+                    cClear('tfFeedback', 'Objects not found! Check the namespace!', [.3, 0, 0])  
+
+        else:
+            cmds.setAttr(nmSpcFX + collGrp + '.v', coll)
+            children = cmds.listRelatives(nmSpcFX + collGrp, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', coll)
+
+            cmds.setAttr(nmSpcFX + animGrp + '.v', anim)
+            children = cmds.listRelatives(nmSpcFX + animGrp, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', anim)
+
+            cmds.setAttr(nmSpcFX + animGrpHi + '.v', animHi)
+            children = cmds.listRelatives(nmSpcFX + animGrpHi, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', animHi)
+
+            cmds.setAttr(nmSpcFX + wrapGrp + '.v', wrap)
+            children = cmds.listRelatives(nmSpcFX + wrapGrp, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', wrap)
+
+            cmds.setAttr(nmSpcFX + exClothGrp + '.v', exCloth)
+            children = cmds.listRelatives(nmSpcFX + exClothGrp, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', exCloth)
+
+            cmds.setAttr(nmSpcFX + xGenBaseGrp + '.v', xGenBase)
+            children = cmds.listRelatives(nmSpcFX + xGenBaseGrp, c=1, type = 'transform')
+            for child in children:
+                cmds.setAttr(child + '.v', xGenBase)
+
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', cape)
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', shirt)
+            
+
+
+
+
+
+
+def tkTglAll(object, *args):
+    listFX    = ['cbAnimGRP', 'cbAnimHiGRP', 'cbColliderGRP', 'cbShirt', 'cbCape', 'cbWrapGRP', 'cbExportClothGRP', 'cbxGenBase']
+    listAnim  = ['cbLow', 'cbMid', 'cbHgh', 'cbButtons']
+    
+    if object is 'FX':
+        state = cmds.checkBox(listFX[0], v=1, q=1) -1
+        for i in listFX:
+            cmds.checkBox(i, v=state, e=1)
+
+    if object is 'Anim':
+        state = cmds.checkBox(listAnim[0], v=1, q=1) -1
+        for i in listAnim:
+            cmds.checkBox(i, v=state, e=1)
+
+
+
+def cSelect(object, *args):
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1)
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if cmds.objExists(nmSpcFX + object) is False:
+                cClear('tfFeedback', 'Objects not found! Check the namespace!', [.3, 0, 0])   
+    
+    else:
+        cmds.select(nmSpcFX + object, r=1)
+
+
+
+def cSelectSimELements(object, *args):
+    CLT = []
+
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1)
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if cmds.objExists(nmSpcFX + 'shirt_cn_cfx_mid_geo_SIM') is False:            
+                cClear('tfFeedback', 'Objects not found! Check the namespace!', [.3, 0, 0])  
+
+    else:
+        if (object == 'shirtMid'):
+            CLT = ['shirt_cn_cfx_mid_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', 1)
+
+        if (object == 'capeMid'):
+            CLT = ['cape_cn_cfx_mid_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', 1)
+            cmds.checkBox('cbCape', v=1, e=1)
+
+        if (object == 'bothMid'):
+            CLT = ['cape_cn_cfx_mid_geo_SIM', 'shirt_cn_cfx_mid_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', 1)
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', 1)
+            cmds.checkBox('cbCape', v=1, e=1)
+            cmds.checkBox('cbShirt', v=1, e=1)
+
+        if (object == 'shirtHi'):
+            CLT = ['shirt_cn_cfx_hi_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', 2)
+            cmds.checkBox('cbShirt', v=1, e=1)
+
+        if (object == 'capeHi'):
+            CLT = ['cape_cn_cfx_hi_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', 2)
+            cmds.checkBox('cbCape', v=1, e=1)
+
+        if (object == 'bothHi'):
+            CLT = ['cape_cn_cfx_hi_geo_SIM', 'shirt_cn_cfx_hi_geo_SIM']
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', 2)
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', 2)
+            cmds.checkBox('cbCape', v=1, e=1)
+            cmds.checkBox('cbShirt', v=1, e=1)
+
+        if (object == 'none'):
+            CLT = []
+            cmds.setAttr(nmSpcFX + dynControl + '.shirt', 0)
+            cmds.setAttr(nmSpcFX + dynControl + '.cape', 0)
+            cmds.checkBox('cbCape', v=0, e=1)
+            cmds.checkBox('cbShirt', v=0, e=1)
+
+
+
+
+
+        cmds.select(clear=1)
+        if nmSpcFX:
+            nmSpcFX = nmSpcFX + ':'
+            for clt in CLT:
+                cmds.select(nmSpcFX + clt, add=1)
+
+        else:
+            cmds.select(CLT, r=1)
+
+
+
+def cSelectFXAnimELements(*args):
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1)
+    grp = 'ANIM_GRP'
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if cmds.objExists(nmSpcFX + grp) is False:
+                cClear('tfFeedback', 'Objects not found! Check the namespace!', [.3, 0, 0])  
+
+    else:
+        cmds.select(nmSpcFX + grp, r=1)
+        cmds.select(nmSpcFX + 'xGenBase_GRP', add=1)
+
+
+def cExportAsABC(*args):
+    mel.eval('AlembicExportSelectionOptions')
+
+
+
+def cReferenceAnimABC(*args):
+    mel.eval(' projectViewer AlembicReference')
+   
+
+
+def cClothCache(*args):
+    mel.eval('doCreateNclothCache 5 { "2", "1", "10", "OneFile", "1", "","0","","0", "0", "0", "1", "1","0","1","mcx" }') 
+
+
+
+def cDeleteCache(*args):
+    mel.eval('fluidDeleteCache')
+
+
+
+def cWrapStatus(action, field, attribute, *args):
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1)
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if action == 'read':
+        sl = cmds.getAttr(nmSpcFX + dynControl + '.' + attribute) +1
+        # print sl
+        cmds.radioButtonGrp(field, sl=sl, e=1)
+
+    if action is 'set':
+        sl = cmds.radioButtonGrp(field, sl=1, q=1) -1
+        # print sl
+        cmds.setAttr(nmSpcFX + dynControl + '.' + attribute, sl)
+        
+    # print '--------------'
+
+
+
+
+def cGetPath(action, *args):
+    defaultPath = "X:/zorro_zor-5069/_library/assets/characters/chr_zorroFox/cfx_groom/"
+
+    if action == 'default':
+        cmds.textField('tfPathxGen', tx=defaultPath, e=1)
+        cmds.textField('xGenVersion', tx=approvedxGen, e=1)
+
+
+    if action == 'choose':
+        xGenPath = cmds.fileDialog2(fm=2, ds=1, dir=defaultPath, cap='Select Directory')
+        cmds.textField('tfPathxGen', tx=xGenPath[0] + '/', e=1)
+        files = cmds.getFileList(filespec = '*.xgen', fld=xGenPath[0])
+
+        if files:
+            if approvedxGen not in files:
+               cmds.textField('xGenVersion', tx='Choose a xGen Collection!', e=1)
+
+        else:
+            cmds.textField('xGenVersion', tx='No xGen descriptions in that Folder!', e=1)
+
+
+
+
+def cSelectxGenVersion(action, *args):
+    xGenPath    = cmds.textField('tfPathxGen', tx=1, q=1)
+    cmdAppend   = '['
+    if xGenPath:
+        files = cmds.getFileList(filespec = '*.xgen', fld=xGenPath)
+        files = sorted(files)
+
+        if (cmds.window('win_xGenList', exists=1)):
+            cmds.deleteUI('win_xGenList')
+        myVersionWindow = cmds.window('win_xGenList', t=('Fox xGen Versions'), s=1)
+
+        cmds.columnLayout(adj=1)
+
+        amount = len(files)
+        if amount > 0:
+            for i in range(0, amount-1, 1):
+                name = files[i].split('.')[0]
+                if i != amount-2:
+                    cmdAppend += '"'
+                    cmdAppend += name   
+                    cmdAppend += '",'
+                else:
+                    cmdAppend += '"'
+                    cmdAppend += name
+                    cmdAppend += '"]'
+
+            cmd1 = 'cmds.textScrollList("txGenVersion", numberOfRows=8, append= '
+            cmd2 = ')'
+            cmd = cmd1 + cmdAppend + cmd2
+
+            cmds.paneLayout()
+            exec(cmd)
+            cmds.setParent('..')
+            cmds.button(l='Pick Version', bgc=(colDarkGreen[0], colDarkGreen[1], colDarkGreen[2]), c=partial(tfSelectxGenVersion))
+
+            cmds.showWindow(myVersionWindow)
+            cmds.window(myVersionWindow, e=1,h=120)
+
+        else:
+            cmds.textField('xGenVersion', tx='No xGen descriptions in that Folder!', e=1)
+
+
+
+def tfSelectxGenVersion(*args):
+    chosenVersion = cmds.textScrollList('txGenVersion', si=1, q=1)
+    cmds.deleteUI('win_xGenList')
+    cmds.textField('xGenVersion', tx=chosenVersion[0], e=1)
+
+
+
+def cImportxGen(action, *args):
+    path    = cmds.textField('tfPathxGen', tx=1, q=1)
+    file    = cmds.textField('xGenVersion', tx=1, q=1)
+    nmSpcFX = cmds.textField('tfNmSpcFX', tx=1, q=1) 
+    
+    if nmSpcFX:
+        nmSpcFX = nmSpcFX + ':'
+
+    if cmds.objExists(nmSpcFX + xGenBaseGeo):
+        pass
+
+
+def cClear(field, text, color, *args):
+    cmds.textField(field, tx=text, bgc=color, e=1)
+
+
+
+
+
+def tkVirtualProductionHelperUI(*args):
+    if (cmds.window('win_tkVirtualProductionHelper', exists=1)):
+        cmds.deleteUI('win_tkVirtualProductionHelper')
+    myWindow = cmds.window('win_tkVirtualProductionHelper', t=('Virtual Production Helper' + ver), s=1)
+
+
+    cmds.columnLayout(adj=1, bgc=(colDarkGreen[0], colDarkGreen[1], colDarkGreen[2]))
+
+
+    # name space
+    # cmds.rowColumnLayout(nc=4, cw = [(1, 120), (2, 90), (3, 120), (4, 90)])
+    # cmds.button(l='nmSpc FX Rig >>', h=bh1, c=partial(cGetNmSpc, 'tfNmSpcFX'), bgc=(colBrown[0], colBrown[1], colBrown[2]))
+    # cmds.textField('tfNmSpcFX', tx='zor_01', bgc=(0,0,0), ed=0)
+
+    # cmds.button(l='nmSpc Anim >>', h=bh1, c=partial(cGetNmSpc, 'tfNmSpcAnim'), bgc=(colBlue[0], colBlue[1], colBlue[2]))
+    # cmds.textField('tfNmSpcAnim', tx='anim', bgc=(0,0,0), ed=0)
+    # cmds.setParent(top=1)
+
+
+
+
+    # visibility
+    cmds.frameLayout('flBuild', l='BuildSkel', bgc=(colFrameGreen[0], colFrameGreen[1], colFrameGreen[2]), cll=1, cl=0, cc=partial(cShrinkWin, "win_tkVirtualProductionHelper"))
+    cmds.rowColumnLayout(nc=4, cw = [(1, 105), (2, 105), (3, 105), (4, 105)])
+
+    cmds.button(l='Build Skeleton', h=bh1, bgc=(colBrown[0], colBrown[1], colBrown[2]), c=partial(tkTglAll, 'FX'))
+
+ 
+
+
+    cmds.showWindow(myWindow)
+    cmds.window('win_tkVirtualProductionHelper', e=1, w=300)
+
+
+
+
+tkVirtualProductionHelperUI()
+cShrinkWin('win_tkVirtualProductionHelper')
+
